ea6b483c266efce0da483e034664e2b0a62821c1
Moonwalkert3ch <6620@holbertonstudents.com>
2025-05-28 10:53:20 -0500

    created next app and configured tailwind. must use nvm 20

---
+++ vys-app/app/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">VYS</h1><br />
+      <h2 className="text-2xl">Prototype</h2>
+  
+    </main>
+  );
+}
---
+++ vys-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // using the `app` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
