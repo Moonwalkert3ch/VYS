3a3ba8a238913ff3886014c351252e25ac88bea3
PuzzleEmptyM <emptympuzzle@gmail.com>
2025-06-05 13:36:06 -0500

    tailwind

--- vys-app/tailwind.config.js
+++ vys-app/tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         outfit: ["var(--font-outfit)", "sans-serif"],
       },
+      width: {
+        'logo': '8rem',
+        'glasses': '16rem',
+      },
+      height: {
+        // add custom heights here if needed
+      },
     },
   },
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}", // using the `src/app` directory
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
-}
+};
