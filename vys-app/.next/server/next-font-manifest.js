9822a2daede1d5ac032ddc7bad9fe03deb4ea039
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2025-06-17 15:46:55 -0500

    Delete vys-app/.next directory

--- vys-app/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/meisibley/VYS/VYS/vys-app/src/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\",\"static/media/c7f848bed9dd25a3-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
